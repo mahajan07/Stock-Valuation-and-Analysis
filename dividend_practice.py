833ae0103c27447985b68e0436bc3a9431689b7e
Dewank Mahajan <66015234+mahajan07@users.noreply.github.com>
2025-06-05 19:45:36 -0400

    Update dividend_practice.py

--- dividend_practice.py
+++ dividend_practice.py
@@ -8,7 +8,6 @@ from collections import namedtuple
 
 Dividend = namedtuple('Dividend', 'timestamp value')
 
-
 class Stock(object):
     def __init__(self, ticker):
             self._name = ticker.upper()
